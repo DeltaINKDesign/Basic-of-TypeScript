f6720b978746431060d1f7ade2ffb0b00710203a
Delta Ink Design <izula97@gmail.com>
2019-02-19 08:40:56 +0100

    Create Custom Types.ts

---
+++ Custom Types.ts
@@ -0,0 +1,9 @@
+type person = {firstName: string};
+
+const example1: object = undefined;
+
+const example2: Object = NaN;
+
+const example3: person = {firstName: 'Sii'};
+
+example3.firstName = 'Si';
